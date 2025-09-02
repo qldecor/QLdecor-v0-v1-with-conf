97431c836ed454d77eb33a8fc1dd292326792af5
Damian Jankowski <damianjankowski111@gmail.com>
2025-09-02 15:32:56 +0200

    change hover effect on navbar mega menu

--- app/[locale]/components/navbar.tsx
+++ app/[locale]/components/navbar.tsx
@@ -57,23 +57,33 @@ export default function Navbar() {
                   <div className="mx-auto max-w-7xl px-12 py-10 grid grid-cols-4 gap-12 text-sm">
                     {/* Płyty meblowe */}
                     <div>
-                      <h3 className="font-semibold mb-4 uppercase">{t('boards')}</h3>
-                      <ul className="space-y-2">
-                        <li><Link href="/products/boards/metalux">MetaLux</Link></li>
-                        <li><Link href="/products/boards/colorpro">ColorPro</Link></li>
-                        <li><Link href="/products/boards/woodsense">WoodSense</Link></li>
+                      <div className="navbar-slide-link w-fit">
+                        <Link
+                          href="/products/handles"
+                          className="linksss group relative z-10 block px-1 py-1 text-gray-700 font-light tracking-wide transition-colors duration-300 hover:text-gray-900 uppercase"
+                        >
+                          {/* <span className="navbar-slide-link"> */}
+                          <span className="main-text">{t('boards')}</span>
+                          <span className="see-all">{t('see-all')}</span>
+                          {/* </span> */}
+                        </Link>
+                      </div>
+                      <ul className="space-y-2 px-1 py-2">
+                        <li><Link className="relative after:content-[''] after:absolute after:left-0 after:-bottom-0.5 after:w-0 after:h-[1px] after:bg-black/30 after:transition-all after:duration-500 after:ease-in-out hover:after:w-full" href="/products/boards/metalux">MetaLux</Link></li>
+                        <li><Link className="relative after:content-[''] after:absolute after:left-0 after:-bottom-0.5 after:w-0 after:h-[1px] after:bg-black/30 after:transition-all after:duration-500 after:ease-in-out hover:after:w-full" href="/products/boards/colorpro">ColorPro</Link></li>
+                        <li><Link  className="relative after:content-[''] after:absolute after:left-0 after:-bottom-0.5 after:w-0 after:h-[1px] after:bg-black/30 after:transition-all after:duration-500 after:ease-in-out hover:after:w-full" href="/products/boards/woodsense">WoodSense</Link></li>
                       </ul>
                     </div>
 
                     {/* Uchwyty */}
-                    <div className="navbar-slide-link">
+                    <div className="navbar-slide-link w-fit">
                       <Link
                         href="/products/handles"
                         className="linksss group relative z-10 block px-1 py-1 text-gray-700 font-light tracking-wide transition-colors duration-300 hover:text-gray-900 uppercase"
                       >
                         {/* <span className="navbar-slide-link"> */}
-                        <span className="main-text">Uchwyty Meblowe</span>
-                        <span className="see-all">SEE ALL</span>
+                        <span className="main-text">{t('handles')}</span>
+                        <span className="see-all">{t('see-all')}</span>
                         {/* </span> */}
                       </Link>
                     </div>
